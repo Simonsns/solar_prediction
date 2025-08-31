61b7747f31c252ea556d8cf74c07c21599cf98b5
Simon.sngs <simon.sngs@hotmail.com>
2025-08-31 21:58:02 +0200

    feat: update solar fetching data

--- src/data_pipeline/data_collection/solar/fetching_data.py
+++ src/data_pipeline/data_collection/solar/fetching_data.py
@@ -1,14 +1,13 @@
 #%% Librairies
 import requests
 import pandas as pd
-from retry_requests import retry
+from typing import Dict, List
 import json
 import logging
-import datetime
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 #%%
-def fetch_solar_production_data(url) -> pd.DataFrame:
-	"""Fonction basique de requête à un URL api, qui retourne les données en format dictionnaire
+def fetch_solar_data(url: str, columns: List[str] | None = None, params: Dict | None = None) -> pd.DataFrame:
+	"""Fonction basique de requête à un URL api, qui retourne les données solaire en format dictionnaire
 
 	Args:
 		url (str): url de l'api
@@ -17,20 +16,15 @@ def fetch_solar_production_data(url) -> pd.DataFrame:
 	Returns:
 		data (dict): données sous forme de dictionnaire
 	"""
-	now = datetime.datetime.today()
-	day = now.strftime("%Y-%m-%d")
-	solar_params = {"select":"code_insee_region, date, heure, solaire", 
-					"where" : f"code_insee_region=76 AND date='{day}'",
-					"order_by":"heure", 
-					"limit": "96"}
 	try:
-		response = requests.get(url, params=solar_params)
+		response = requests.get(url, params=params) 
 		response.raise_for_status()
 		json_data = response.json()
-		data = pd.DataFrame(json_data.get("results"))
-		logging.info("Production solaire en temps réel téléchargée avec succès")
-		return data 
-	
+		data = pd.DataFrame(data=(json_data.get("results", [])), columns=columns)
+
+		logging.info("Data solaire téléchargée avec succès")
+		return data
+		
 	except requests.RequestException as e:
 		logging.error(f"Erreur lors de la requête API: {e}")
 		raise
